1acf10a7dc4c213034fb0c5e45c954ba38034e4b
David Horwitz <dhorwitz@gmail.com>
2007-09-05 11:30:44 +0000

    Move jobs inside the project and register them
    git-svn-id: https://source.sakaiproject.org/contrib/turnitin/trunk@11741 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ contentreview-impl/scheduler-jobs/src/java/org/sakaiproject/component/app/scheduler/jobs/ContentReviewReports.java
@@ -0,0 +1,24 @@
+package org.sakaiproject.component.app.scheduler.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import org.sakaiproject.contentreview.service.ContentReviewService;
+
+public class ContentReviewReports implements Job {
+
+	private ContentReviewService contentReviewService;
+	public void setContentReviewService(ContentReviewService sd){
+		contentReviewService = sd;
+		
+
+	}
+	
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		// TODO Auto-generated method stub
+		//contentReviewService.processQueue();
+		contentReviewService.checkForReports();
+	}
+
+}
