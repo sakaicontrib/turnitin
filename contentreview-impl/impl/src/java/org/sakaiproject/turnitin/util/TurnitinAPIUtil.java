f38d79ba5ce551b5ed55622b921306102afb2605
David Horwitz <dhorwitz@gmail.com>
2010-07-22 08:37:14 +0000

    TII-89 add copyright headers
    git-svn-id: https://source.sakaiproject.org/contrib/turnitin/trunk@69347 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- contentreview-impl/impl/src/java/org/sakaiproject/turnitin/util/TurnitinAPIUtil.java
+++ contentreview-impl/impl/src/java/org/sakaiproject/turnitin/util/TurnitinAPIUtil.java
@@ -1,3 +1,23 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/turnitin/trunk/contentreview-impl/impl/src/java/org/sakaiproject/contentreview/impl/turnitin/TurnitinReviewServiceImpl.java $
+ * $Id: TurnitinReviewServiceImpl.java 69345 2010-07-22 08:11:44Z david.horwitz@uct.ac.za $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2006 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
 package org.sakaiproject.turnitin.util;
 
 import java.io.BufferedReader;
