414fb20317325cd82d7a3408015959c44c0bb6cb
Steven Githens <swgithen@mtu.edu>
2011-03-30 20:23:58 +0000

    NOJIRA Not a big fan of the hard tabs, but reformatting this file so it matches the rest of the project using hard tabs.
    git-svn-id: https://source.sakaiproject.org/contrib/turnitin/trunk@73452 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- contentreview-impl/impl/src/java/org/sakaiproject/contentreview/impl/turnitin/TurnitinSessionFuncs.java
+++ contentreview-impl/impl/src/java/org/sakaiproject/contentreview/impl/turnitin/TurnitinSessionFuncs.java
@@ -27,66 +27,66 @@ import org.w3c.dom.Element;
 public class TurnitinSessionFuncs {
 	private static final Log log = LogFactory.getLog(TurnitinSessionFuncs.class);
 
-    /**
-     * Logs in to turnitin.  Scrapes the response XML and returns the session id
-     * or throws an exception if it was unable to log in.
-     * 
-     * @param conn
-     * @param params This function will take care of the fids/fcmds. Just make
-     * sure this has the defaults you use for every TII call as well as the 
-     * user info including utp (even though utp doesn't seem to make sense 
-     * since this doesn't include a course id parameter.)
-     * @return session-id for use in other turnitin calls.
-     * @throws TransientSubmissionException
-     * @throws SubmissionException 
-     */
-    public static String getTurnitinSession(TurnitinAccountConnection conn,
-            @SuppressWarnings("rawtypes") Map params) throws 
-            TransientSubmissionException, SubmissionException {
-        Map finalParams = new HashMap();
-        finalParams.putAll(params);
-        finalParams.put("fid", "17");
-        finalParams.put("fcmd", "2");
-        
-        Document doc = conn.callTurnitinReturnDocument(finalParams);
-        
-        Element root = doc.getDocumentElement();
-        int rcode = new Integer(((CharacterData) 
-                (root.getElementsByTagName("rcode").item(0).getFirstChild())).getData().trim()).intValue();
-        String message = ((CharacterData) 
-                (root.getElementsByTagName("rmessage").item(0).getFirstChild())).getData().trim();
-        
-        if (!(rcode > 0 && rcode < 100)) {
-            throw new TransientSubmissionException("Error logging in to turnitin: " + message);
-        }
-        
-        String sessionId = ((CharacterData) 
-                (root.getElementsByTagName("sessionid").item(0).getFirstChild())).getData().trim();
-        
-        log.debug("Log in results. rcode: " + rcode + " message: " + message +
-        		" sessionId: " + sessionId);
-        
-        return sessionId;
-    }
-    
-    /**
-     * Logs out of a turnitin session. The params map should contain the same 
-     * information as in {@link TurnitinSessionFuncs.getTurnitinSession}
-     * 
-     * @param conn
-     * @param sessionId
-     * @param params
-     * @throws TransientSubmissionException
-     * @throws SubmissionException
-     */
-    public static void logoutTurnitinSession(TurnitinAccountConnection conn,
-            String sessionId, Map params) throws TransientSubmissionException, SubmissionException {
-        Map finalParams = new HashMap();
-        finalParams.putAll(params);
-        finalParams.put("fid", "18");
-        finalParams.put("fcmd", "2");
-        finalParams.put("session-id", sessionId);
-        
-        conn.callTurnitinReturnDocument(finalParams);
-    }
+	/**
+	 * Logs in to turnitin.  Scrapes the response XML and returns the session id
+	 * or throws an exception if it was unable to log in.
+	 * 
+	 * @param conn
+	 * @param params This function will take care of the fids/fcmds. Just make
+	 * sure this has the defaults you use for every TII call as well as the 
+	 * user info including utp (even though utp doesn't seem to make sense 
+	 * since this doesn't include a course id parameter.)
+	 * @return session-id for use in other turnitin calls.
+	 * @throws TransientSubmissionException
+	 * @throws SubmissionException 
+	 */
+	public static String getTurnitinSession(TurnitinAccountConnection conn,
+			@SuppressWarnings("rawtypes") Map params) throws 
+			TransientSubmissionException, SubmissionException {
+		Map finalParams = new HashMap();
+		finalParams.putAll(params);
+		finalParams.put("fid", "17");
+		finalParams.put("fcmd", "2");
+
+		Document doc = conn.callTurnitinReturnDocument(finalParams);
+
+		Element root = doc.getDocumentElement();
+		int rcode = new Integer(((CharacterData) 
+				(root.getElementsByTagName("rcode").item(0).getFirstChild())).getData().trim()).intValue();
+		String message = ((CharacterData) 
+				(root.getElementsByTagName("rmessage").item(0).getFirstChild())).getData().trim();
+
+		if (!(rcode > 0 && rcode < 100)) {
+			throw new TransientSubmissionException("Error logging in to turnitin: " + message);
+		}
+
+		String sessionId = ((CharacterData) 
+				(root.getElementsByTagName("sessionid").item(0).getFirstChild())).getData().trim();
+
+		log.debug("Log in results. rcode: " + rcode + " message: " + message +
+				" sessionId: " + sessionId);
+
+		return sessionId;
+	}
+
+	/**
+	 * Logs out of a turnitin session. The params map should contain the same 
+	 * information as in {@link TurnitinSessionFuncs.getTurnitinSession}
+	 * 
+	 * @param conn
+	 * @param sessionId
+	 * @param params
+	 * @throws TransientSubmissionException
+	 * @throws SubmissionException
+	 */
+	public static void logoutTurnitinSession(TurnitinAccountConnection conn,
+			String sessionId, Map params) throws TransientSubmissionException, SubmissionException {
+		Map finalParams = new HashMap();
+		finalParams.putAll(params);
+		finalParams.put("fid", "18");
+		finalParams.put("fcmd", "2");
+		finalParams.put("session-id", sessionId);
+
+		conn.callTurnitinReturnDocument(finalParams);
+	}
 }
