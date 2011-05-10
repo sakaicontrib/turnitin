5fa975f7dbd26030588069209318f00d143c5eb7
Damian Sobiera <dsobiera@indiana.edu>
2011-05-10 17:19:52 +0000

    TII-109 commented out return that would prevent submissions with file extensions that TII doesn't support from being submitted
    git-svn-id: https://source.sakaiproject.org/contrib/turnitin/trunk@74128 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- contentreview-impl/impl/src/java/org/sakaiproject/contentreview/impl/turnitin/TurnitinContentValidator.java
+++ contentreview-impl/impl/src/java/org/sakaiproject/contentreview/impl/turnitin/TurnitinContentValidator.java
@@ -152,9 +152,11 @@ public class TurnitinContentValidator {
 			}
 		}
 
-		if (!fileTypeOk) {
-			return false;
-		}
+        // for files like .png we'd like to get a status code from TII so we can display an error message
+        // other than "An unknown error occurred"
+//      if (!fileTypeOk) {
+//          return false;
+//      }
 
 		//TODO: if file is too big reject here 10.48576 MB
 
