14d7e741c82a852acd096ae8a433ea500ff5f312
abell@turnitin.com <abell@turnitin.com@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2012-07-17 15:44:55 +0000

    TII-129 Added Support for GradeMark, e-rater assignment options, multiple instructors and Open API migration
    git-svn-id: https://source.sakaiproject.org/contrib/turnitin/branches/TII-129@80782 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- contentreview-impl/impl/src/java/org/sakaiproject/turnitin/util/TIIFID.java
+++ contentreview-impl/impl/src/java/org/sakaiproject/turnitin/util/TIIFID.java
@@ -21,59 +21,62 @@
 package org.sakaiproject.turnitin.util;
 
 public enum TIIFID {
-    fid1 (1, new TIIParam[] {TIIParam.aid, TIIParam.diagnostic, TIIParam.dis, 
-            TIIParam.encrypt, TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime, 
-            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid, TIIParam.uln, 
+    fid1 (1, new TIIParam[] {TIIParam.aid, TIIParam.diagnostic, TIIParam.dis,
+            TIIParam.encrypt, TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime,
+            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid, TIIParam.uln,
             TIIParam.upw, TIIParam.username, TIIParam.utp }),
-    fid2 (2, new TIIParam[] {TIIParam.aid, TIIParam.ced, TIIParam.cid, 
+    fid2 (2, new TIIParam[] {TIIParam.aid, TIIParam.ced, TIIParam.cid,
             TIIParam.cpw, TIIParam.ctl, TIIParam.diagnostic, TIIParam.dis,
             TIIParam.encrypt, TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime,
-            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid, 
+            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid,
             TIIParam.uln, TIIParam.upw, TIIParam.username, TIIParam.utp}),
-    fid3 (3, new TIIParam[] { 
+    fid3 (3, new TIIParam[] {
             TIIParam.cid, TIIParam.ctl, // These two are not in the spec on page48
             TIIParam.aid, TIIParam.diagnostic, TIIParam.dis, TIIParam.encrypt,
-            TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime, TIIParam.said, 
+            TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime, TIIParam.said,
             TIIParam.tem, TIIParam.uem, TIIParam.ufn, TIIParam.uid, TIIParam.uln,
             TIIParam.upw, TIIParam.username, TIIParam.utp}),
     fid4 (4, new TIIParam[] {TIIParam.aid, TIIParam.assign, TIIParam.assignid,
             TIIParam.ced, TIIParam.cid, TIIParam.ctl, TIIParam.diagnostic,
             TIIParam.dis, TIIParam.dtdue, TIIParam.dtstart, TIIParam.encrypt,
             TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime, TIIParam.newassign,
-            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid, 
+            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid,
             TIIParam.uln, TIIParam.upw, TIIParam.username, TIIParam.utp}),
     fid5 (5, new TIIParam[] {
             TIIParam.aid, TIIParam.assign, TIIParam.assignid, TIIParam.cid,
             TIIParam.ctl, TIIParam.diagnostic, TIIParam.dis, TIIParam.encrypt,
-            TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime, TIIParam.pfn, 
+            TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime, TIIParam.pfn,
             TIIParam.pln, TIIParam.ptl, TIIParam.ptype, TIIParam.said,
             TIIParam.tem, TIIParam.uem, TIIParam.ufn, TIIParam.uid, TIIParam.uln,
             TIIParam.username, TIIParam.utp}),
     fid6 (6, new TIIParam[] {
-            TIIParam.aid, TIIParam.assignid, TIIParam.cid, TIIParam.diagnostic, 
-            TIIParam.dis, TIIParam.encrypt, TIIParam.fcmd, TIIParam.fid, 
-            TIIParam.gmtime, TIIParam.oid, TIIParam.said, TIIParam.tem, 
-            TIIParam.uem, TIIParam.ufn, TIIParam.uid, TIIParam.uln, 
+            TIIParam.aid, TIIParam.assignid, TIIParam.cid, TIIParam.diagnostic,
+            TIIParam.dis, TIIParam.encrypt, TIIParam.fcmd, TIIParam.fid,
+            TIIParam.gmtime, TIIParam.oid, TIIParam.said, TIIParam.tem,
+            TIIParam.uem, TIIParam.ufn, TIIParam.uid, TIIParam.uln,
             TIIParam.username, TIIParam.utp}),
     fid10 (10, new TIIParam[] {TIIParam.aid, TIIParam.assign, TIIParam.assignid,
-            TIIParam.cid, TIIParam.ctl, TIIParam.diagnostic, TIIParam.dis, 
+            TIIParam.cid, TIIParam.ctl, TIIParam.diagnostic, TIIParam.dis,
             TIIParam.encrypt, TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime,
             TIIParam.said, TIIParam.tem, TIIParam.uem, TIIParam.ufn, TIIParam.uid,
             TIIParam.uln, TIIParam.username, TIIParam.utp }),
     fid17 (17, new TIIParam[] {TIIParam.aid, TIIParam.diagnostic, TIIParam.dis,
             TIIParam.encrypt, TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime,
-            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid, 
+            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid,
             TIIParam.uln, TIIParam.upw, TIIParam.username, TIIParam.utp}),
     fid18 (18, new TIIParam[] {TIIParam.aid, TIIParam.diagnostic, TIIParam.dis,
             TIIParam.encrypt, TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime,
-            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid, 
+            TIIParam.said, TIIParam.uem, TIIParam.ufn, TIIParam.uid,
             TIIParam.uln, TIIParam.upw, TIIParam.username, TIIParam.utp}),
     fid19 (19, new TIIParam[] {TIIParam.aid, TIIParam.assign, TIIParam.assignid,
             TIIParam.ced, TIIParam.cid, TIIParam.ctl, TIIParam.diagnostic,
             TIIParam.dis, TIIParam.dtdue, TIIParam.dtstart, TIIParam.encrypt,
             TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime, TIIParam.newassign,
-            TIIParam.said, TIIParam.tem, TIIParam.uem, TIIParam.ufn, TIIParam.uid, 
-            TIIParam.uln, TIIParam.upw, TIIParam.username, TIIParam.utp});
+            TIIParam.said, TIIParam.tem, TIIParam.uem, TIIParam.ufn, TIIParam.uid,
+            TIIParam.uln, TIIParam.upw, TIIParam.username, TIIParam.utp}),
+    fid99 (99, new TIIParam[] {TIIParam.aid, TIIParam.diagnostic, TIIParam.encrypt,
+            TIIParam.fcmd, TIIParam.fid, TIIParam.gmtime,
+            TIIParam.uem, TIIParam.ufn, TIIParam.uln,TIIParam.utp});
     /*
     5,
     6,
@@ -90,7 +93,7 @@ public enum TIIFID {
         this.fidnum = fidnum;
         this.md5params = md5params;
     }
-    
+
     public boolean includeParamInMD5(String paramname) {
         boolean togo = false;
         for (TIIParam param: md5params) {
