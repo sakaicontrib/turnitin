d8f0b8d7a2f95a22a9d711645229e11c6d0da373
David Horwitz <dhorwitz@gmail.com>
2012-12-20 08:37:35 +0000

    TII-142 missing class
    git-svn-id: https://source.sakaiproject.org/contrib/turnitin/branches/TII-142@82340 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ contentreview-impl/impl/src/java/org/sakaiproject/contentreview/entity/ContentReviewResult.java
@@ -0,0 +1,81 @@
+package org.sakaiproject.contentreview.entity;
+
+import java.util.Date;
+
+/**
+ * A POJO ot expose the results of a content review item 
+ * in EB
+ * @author dhorwitz
+ *
+ */
+public class ContentReviewResult {
+	
+	
+	private String userEid;
+	private String taskId;
+	private String siteId;
+	
+	private Date dateQueued;
+	private Date dateReportReceived;
+	private Long status;
+	private String reviewScore;
+	private String ErrorCode;
+	private String ErrorMessage;
+	public String getUserEid() {
+		return userEid;
+	}
+	public void setUserEid(String userEid) {
+		this.userEid = userEid;
+	}
+	public String getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+	public String getSiteId() {
+		return siteId;
+	}
+	public void setSiteId(String siteId) {
+		this.siteId = siteId;
+	}
+	public Date getDateQueued() {
+		return dateQueued;
+	}
+	public void setDateQueued(Date dateQueued) {
+		this.dateQueued = dateQueued;
+	}
+	public Date getDateReportReceived() {
+		return dateReportReceived;
+	}
+	public void setDateReportReceived(Date dateReportReceived) {
+		this.dateReportReceived = dateReportReceived;
+	}
+	public Long getStatus() {
+		return status;
+	}
+	public void setStatus(Long status) {
+		this.status = status;
+	}
+	public String getReviewScore() {
+		return reviewScore;
+	}
+	public void setReviewScore(String reviewScore) {
+		this.reviewScore = reviewScore;
+	}
+	public String getErrorCode() {
+		return ErrorCode;
+	}
+	public void setErrorCode(String errorCode) {
+		ErrorCode = errorCode;
+	}
+	public String getErrorMessage() {
+		return ErrorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		ErrorMessage = errorMessage;
+	}
+	
+	
+	
+}
