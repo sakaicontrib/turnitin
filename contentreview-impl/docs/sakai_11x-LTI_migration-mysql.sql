d08891f8c23b2229a09d4769b001daf5a0f36a4b
Brian Baillargeon <bbailla2@uwo.ca>
2017-01-19 16:26:24 -0500

    TII-243 - Remove SakaiBLTIUtil.addTIIproperties in basiclti (#52)

--- contentreview-impl/docs/sakai_11x-LTI_migration-mysql.sql
+++ contentreview-impl/docs/sakai_11x-LTI_migration-mysql.sql
@@ -9,3 +9,8 @@ ALTER TABLE CONTENTREVIEW_ITEM DROP version, DROP providerId;
 
 -- Close off HTTP access to submissions made prior to the deployment date of the LTI code  (TII-240)
 UPDATE CONTENTREVIEW_ITEM SET urlAccessed = 1 WHERE dateSubmitted < SYSDATE();
+
+-- Check if LTI_CONTENT is semicolon or new line delimited and apply one of the following as appropriate (TII-243):
+UPDATE LTI_CONTENT SET CUSTOM = REPLACE(CUSTOM, ';custom_', ';') WHERE tool_id in (select id from lti_tools where SITE_ID in ('!turnitin', '!turnitin_reports'));
+-- OR --
+UPDATE LTI_CONTENT SET CUSTOM = REPLACE(CUSTOM, '\ncustom_', '\n') WHERE tool_id in (select id from lti_tools where SITE_ID in ('!turnitin', '!turnitin_reports'));
